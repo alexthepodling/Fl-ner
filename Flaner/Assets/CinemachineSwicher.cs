2b64c07996271e293a3184aedcb30957c8d1691e
alexthepodling <72024544+alexthepodling@users.noreply.github.com>
2022-01-23 19:36:13 +0000

    Movement in Bakery (W/ ISSUES)
    Unresolved cinemachine issues in this first push of the game; outside of issues, camera movement and third person character movement works

---
+++ Flaner/Assets/CinemachineSwicher.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using Cinemachine;
+
+public class CinemachineSwicher : MonoBehaviour
+{
+    [SerializeField] private InputAction action;
+
+    private bool playerCam = true;
+
+    private Animator animator;
+
+    [SerializeField] private CinemachineVirtualCamera vcam1; //player
+    [SerializeField] private CinemachineVirtualCamera vcam2; //baking
+
+
+    private void Awake()
+    {
+       //    animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        action.Disable();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        action.performed += _ => SwitchPriority();
+    }
+
+    private void SwitchState()
+    {
+        if(playerCam)
+        {
+            animator.Play("BakingCamera");
+        }
+        else
+        {
+            animator.Play("PlayerCamera");
+        }
+        playerCam = !playerCam;
+    }
+
+    private void SwitchPriority()
+    {
+        if(playerCam)
+        {
+            vcam1.Priority = 0;
+            vcam2.Priority = 1;
+        }
+        else
+        {
+            vcam1.Priority = 1;
+            vcam2.Priority = 0;
+        }
+        playerCam = !playerCam;
+    }
+}
